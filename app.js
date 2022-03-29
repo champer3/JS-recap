3684949aa6cbe7c8cc1e2c04423d34f68d2927da
champer3 <okunolas21@gmail.com>
2022-03-29 08:19:54 -0700

    js recap for switch statements

--- app.js
+++ app.js
@@ -26,3 +26,37 @@
 
 // const maxNr = Math.max(1, 7)
 // console.log(maxNr)
+
+// function max(nr1 = 0, nr2 = 0){
+//     if(nr1 >= nr2){
+//         return nr1
+//     }else{
+//         return nr2
+//         // console.log(nr2)
+//     }
+// }
+
+// let inbox = max(10, 15)
+// console.log(inbox)
+
+//ARRAYS
+
+// const listUsers = ["stephen", "Bolu", "Biggy"]
+
+// listUsers.push("Angel")
+
+// console.log(listUsers)
+
+//SWITCH STATEMENT
+
+// const text = 'bananas'
+// switch(text){
+//     case 'bananas':
+//         console.log('wow i love bananas')
+//         break
+//     case 'apple':
+//         console.log('I love apple')
+//         break
+//     default:
+//         console.log('I dont know this fruit')
+// }
