24c6ffc8067d97116def3f434379e1332a42bf40
champer3 <okunolas21@gmail.com>
2022-03-28 23:53:53 -0700

    js-recap

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const age = 20
+// const age = 20
 
-if(age >= 18){
-    console.log("You are an adult")
-}else{
-    console.log("Come back next year")
-}
+// if(age >= 18){
+//     console.log("You are an adult")
+// }else{
+//     console.log("Come back next year")
+// }
 
+// FALSEY VALUES IN JS
+//Fase, 0, "", null, undefined, NaN
+
+//FUNCTIONS
+
+// function greet(name){
+//     console.log("welcome to the team " + name)
+// }
+
+// function signUp(){
+//     let name = prompt("What is your name?")
+//     greet(name)
+// }
+// signUp()
+
+// console.log("the rest of the code")
+
+
+// const maxNr = Math.max(1, 7)
+// console.log(maxNr)
